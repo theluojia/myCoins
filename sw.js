5e2d4e053ee7fd22dc8a1f8681c5803a00a4efe9
Kai Li <likai@softsec-tech.cn>
2026-05-14 19:22:05 +0800

    claudecode changes

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'mycoins-v1';
+const STATIC_ASSETS = [
+  './',
+  './index.html',
+  './styles.css',
+  './coin_data.json',
+  './matrix.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(STATIC_ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) =>
+      cached || fetch(event.request).then((response) => {
+        if (response.ok) {
+          const clone = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+        }
+        return response;
+      })
+    ).catch(() => caches.match('./index.html'))
+  );
+});
